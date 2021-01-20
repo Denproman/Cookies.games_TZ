90af5fa465e79fca4f3da4cfd107cfcb76e5f6c7
DESKTOP-83HCQQP\Денис <denis.progger@gmail.com>
2021-01-20 14:04:15 +0200

    Ready Project

---
+++ Assets/Scripts/Outliner.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Outliner : MonoBehaviour
+{
+    [SerializeField] private Outline outline;
+    private float alpha;
+    private bool isOulined;
+    [SerializeField] private float alphaKoef;
+    private void Start()
+    {
+        if (outline == null)
+        {
+            outline = gameObject.GetComponent<Outline>();
+        }
+        outline.effectColor = new Color(0f, 0f, 0f, 0f);
+    }
+
+    private void Update()
+    {
+        
+        if (outline.effectColor.a >= 1)
+        {
+            isOulined = true;   
+        }
+        else if (outline.effectColor.a <= 0) 
+        {
+            isOulined = false;
+        }
+
+        if (!isOulined)
+        {          
+            alpha += alphaKoef;
+        }
+        else
+        {  
+            alpha -= alphaKoef;
+        }
+
+        outline.effectColor = new Color(0f, 0f, 0f, alpha/255);
+    }
+}
---
+++ Assets/Scripts/TankRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankRotation : MonoBehaviour
+{
+    [SerializeField] private float rotateAngle;
+    private void Update()
+    {
+        transform.Rotate(0, rotateAngle, 0);
+    }
+}
